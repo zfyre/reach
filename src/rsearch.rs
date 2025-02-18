24ba2217c43c03fac4eadef275ffacfc23ba0a75
Kshitiz <kshitiz4kaushik@gmail.com>
2025-02-18 23:04:23 +0530

    feat: fixes

--- src/rsearch.rs
+++ src/rsearch.rs
@@ -1,5 +1,11 @@
 /// 1. File to configure the Research module using google search, gemini search and Arxive search
 /// 2. Handles the search even if Api key is not provided using only Arxiv search : LATER!!
+
+
+
+// MKAE THIS OPEN IN A VIM TYPE DIFFERENT GUI IN TERMINAL WHEERE YOU CAN DO VARIOUS TO & FRO COMMS
+
+
 use clap::Parser;
 use scraper::{Html, Selector};
 
