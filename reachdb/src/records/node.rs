9936c83e13ff28fc83ba4e3b12451b89ec404084
Kshitiz <kshitiz4kaushik@gmail.com>
2025-03-05 00:32:26 +0530

    feat: updated function sig of write and read of Record trait

--- reachdb/src/records/node.rs
+++ reachdb/src/records/node.rs
@@ -17,21 +17,76 @@ impl NodeRecord {
 }
 
 impl Record for NodeRecord {
-    fn read(mmap: &MmapMut, offset: usize) -> Result<Self, ReachdbError>
+    fn read(mmap: &MmapMut, id: u64) -> Result<Self, ReachdbError>
     where
         Self: Sized,
-    {
+    {   
+        let offset = id as usize * Self::record_size();
         let end = offset + Self::record_size();
         let data = &mmap[offset..end];
         Ok(bincode::deserialize(data)?)
     }
 
-    fn write(&self, mmap: &mut MmapMut, offset: usize) -> Result<(), ReachdbError> {
+    fn write(&self, mmap: &mut MmapMut) -> Result<(), ReachdbError> {
+        let offset = self.id as usize * Self::record_size();
         let encoded = bincode::serialize(self)?;
-        let end = offset + encoded.len();
+        let end = self.id as usize * Self::record_size() + encoded.len();
+
         mmap[offset..end].copy_from_slice(&encoded);
         mmap.flush()?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::records::{node::NodeRecord, Record};
+
+    #[test]
+    fn writing_node_records() {
+
+        use crate::utils::create_mmap;
+        use memmap2::MmapOptions;
+        use std::fs::OpenOptions;
+
+        let file_path = "reachdb.node.db";
+
+        // Create file and write node
+        {
+            let mut mmap = create_mmap(file_path, 4096).unwrap();
+                for i in 0..100 {
+                    let node = super::NodeRecord {
+                        id: i,
+                        first_relationship_offset: i*2,
+                        first_property_offset: i*i,
+                    };
+                    node.write(&mut mmap).unwrap();
+                }
+            // mmap will be flushed and dropped here
+        }
+
+        // Reopen file and read node
+        {
+            let file = OpenOptions::new()
+                .read(true)
+                .write(true)
+                .open(file_path)
+                .unwrap();
+            let mmap = unsafe { MmapOptions::new()
+                    .map_mut(&file)
+                    .unwrap()
+            };
+            for i in 0..100 {
+                let read_node = NodeRecord::read(&mmap, i).unwrap();
+
+                assert_eq!(read_node.id, i);
+                assert_eq!(read_node.first_relationship_offset, i*2);
+                assert_eq!(read_node.first_property_offset, i*i);
+            }
+        }
+
+        // Clean up
+        // std::fs::remove_file(file_path).unwrap();
     }
 }
--- reachdb/src/records/relationship.rs
+++ reachdb/src/records/relationship.rs
@@ -6,6 +6,7 @@ use super::{Record, ReachdbError};
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct RelationshipRecord {
+    pub id: u64,
     pub source_id: u64,
     pub target_id: u64,
     pub first_property_offset: u64,
@@ -13,15 +14,19 @@ pub struct RelationshipRecord {
 }
 
 impl Record for RelationshipRecord {
-    fn read(mmap: &MmapMut, offset: usize) -> Result<Self, ReachdbError>
+    fn read(mmap: &MmapMut, id: u64) -> Result<Self, ReachdbError>
     where
         Self: Sized,
     {
+        let offset = id as usize * Self::record_size();
+        
         let end = offset + Self::record_size();
         let data = &mmap[offset..end];
         Ok(bincode::deserialize(data)?)
     }
-    fn write(&self, mmap: &mut MmapMut, offset: usize) -> Result<(), ReachdbError> {
+    fn write(&self, mmap: &mut MmapMut) -> Result<(), ReachdbError> {
+        let offset = self.id as usize * Self::record_size();
+
         let encoded = bincode::serialize(self)?;
         let end = offset + encoded.len();
         mmap[offset..end].copy_from_slice(&encoded);
